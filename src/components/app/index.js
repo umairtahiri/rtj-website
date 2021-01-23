cba75263a335f6a2ffb57a83a05e87105a96af8a
Umair Bashir <umairtahiri9212@gmail.com>
2021-01-23 19:39:59 +0500

    main component name changed for avoiding confusion

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -4,11 +4,11 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap-css-only/css/bootstrap.min.css'
 import 'mdbreact/dist/css/mdb.css'
 
-import Home from '../main-stream'
+import MainStream from '../main-stream'
 
 const App = () => (
   <Router>
-    <Home path="/" />
+    <MainStream path="/" />
   </Router>
 )
 
